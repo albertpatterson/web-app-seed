654c8c82f80bba5fa9b1da7138f095d380767e04
Albert Patterson <apatterson189@gmail.com>
2018-05-08 20:43:44 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    app: './assets/app.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: './assets/app.bundle.js'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist/assets']),
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, "./dist"),
+    compress: true,
+    port: 12000,
+    stats: 'errors-only',
+    open: true
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: /src/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['env']
+          }
+        }
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader']
+      },
+      {
+        test:/\.css$/,
+        use:['style-loader','css-loader']
+      },
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: './assets/media/',
+              publicPath: './assets/media/'
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
+
+module.exports = config;
