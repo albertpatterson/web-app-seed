9f3957961566832e16fea7718fc75b0f6861c4d8
Albert Patterson <apatterson189@gmail.com>
2018-05-21 19:57:16 -0400

    update to use sass loader and more easily switch modes

--- src/assets/app.js
+++ src/assets/app.js
@@ -1,4 +1,4 @@
-import './css/app.css'
+import './scss/app.scss'
 
 const message = require("./js/message");
 console.log(message);
--- webpack.config.js
+++ webpack.config.js
@@ -4,74 +4,75 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const devMode = process.env.NODE_ENV !== 'production';
-
-const config = {
-  context: path.resolve(__dirname, 'src'),
-  entry: {
-    app: './assets/app.js'
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: './assets/app.bundle.js'
-  },
-  plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new HtmlWebpackPlugin({
-      template: 'index.html',
-      favicon: 'assets/media/favicon.ico'
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'assets/css/app.css'
-    })
-  ],
-  devServer: {
-    contentBase: path.resolve(__dirname, "./dist"),
-    compress: true,
-    port: 12003,
-    stats: 'errors-only',
-    open: true
-  },
-  devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        include: /src/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ['env']
-          }
-        }
-      },
-      {
-        test: /\.html$/,
-        use: ['html-loader']
-      },
-      {
-        test:/\.css$/,
-        use: [
-          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-          'css-loader'
-        ],
-      },
-      {
-        test: /\.(jpg|png|gif|svg|ico)$/,
-        use: [
-          {
-            loader: 'file-loader',
+module.exports = function(env, argv) {
+  const config = {
+    mode: env.production ? 'production' : 'development',
+    devtool: env.production ? 'source-map' : 'inline-source-map',
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+      app: './assets/app.js'
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: './assets/app.bundle.js'
+    },
+    plugins: [
+      new CleanWebpackPlugin(['dist']),
+      new HtmlWebpackPlugin({
+        template: 'index.html',
+        favicon: 'assets/media/favicon.ico'
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'assets/css/app.css'
+      })
+    ],
+    devServer: {
+      contentBase: path.resolve(__dirname, "./dist"),
+      compress: true,
+      port: 12003,
+      stats: 'errors-only',
+      open: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          include: /src/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
             options: {
-              name: '[name].[ext]',
-              outputPath: './assets/media/',
-              publicPath: './assets/media/'
+              presets: ['env']
             }
           }
-        ]
-      }
-    ]
-  }
-};
+        },
+        {
+          test: /\.html$/,
+          use: ['html-loader']
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            env.production ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader', 'sass-loader'
+          ],
+        },
+        {
+          test: /\.(jpg|png|gif|svg|ico)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+                outputPath: './assets/media/',
+                publicPath: './assets/media/'
+              }
+            }
+          ]
+        }
+      ]
+    }
+  };
 
-module.exports = config;
+  return config;
+}
